20ca4eb8c7fb881ab8ad8af94ce2f52e8eec6fa0
Yash Mishra <96054040+the-y9@users.noreply.github.com>
2024-08-08 12:22:03 +0530

    Update track.php

--- track.php
+++ track.php
@@ -1,10 +1,20 @@
 <?php
+// Path to the log file
 $file = 'ip_log.txt';
-$ip = $_SERVER['REMOTE_ADDR'];
-$date = date('Y-m-d H:i:s');
-$entry = "$date - $ip\n";
-file_put_contents($file,$entry,FILE_APPEND);
-header('Content-Type: image/png');
-readfile('traking_image.png');
 
+// Get the IP address of the client
+$ip = $_SERVER['REMOTE_ADDR'];
+
+// Get the current date and time
+$date = date('Y-m-d H:i:s');
+
+// Format the log entry
+$entry = "$date - $ip\n";
+
+// Append the log entry to the file
+file_put_contents($file, $entry, FILE_APPEND);
+
+// Serve the tracking image
+header('Content-Type: image/png');
+readfile('tracking_image.png');
 ?>
